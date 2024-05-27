51dc1b3444a8ab1b004f14be87b169ecea8e7751
Sydney Andre <s.a.andre0217@gmail.com>
2024-05-27 11:15:26 -0500

    completed euler 1 and added specs

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -6,10 +6,14 @@
 ; http://projecteuler.net/index.php?section=problems&id=1
 
 (describe "Euler Problem #1"
-
+ (it "Output a number"
+    (should= false (boolean? (euler-1 10)))
+    (should= true (number? (euler-1 10)))
+    )
   (it "Solves #1"
+    (should-not= -1 (euler-1 10))
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+	(should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,11 @@
-(ns euler.level1.problem001)
-
+(ns euler.level1.problem001);
+;;If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5, 6 and 9.
+;;The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 (defn euler-1 [n]
-  )
+  (let [multiples (for [i (range 1 n)
+                        :when (or (zero? (mod i 3))
+                                  (zero? (mod i 5)))]
+                    i)
+        sum (reduce + multiples)]
+    sum))
+
