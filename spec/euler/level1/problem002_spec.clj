f6fca277652e27a08ab74c07a80a92bdd6016798
Sydney Andre <s.a.andre0217@gmail.com>
2024-06-03 21:05:20 -0500

    completed euler-2

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -5,10 +5,51 @@
 
 ; https://projecteuler.net/problem=2
 
-#_(describe "Euler Problem #2"
+(describe "Euler Problem #2"
+
+          (it "finds perfect square"
+              (should= true (perfect-square? 0))
+              (should= true (perfect-square? 1))
+              (should= false (perfect-square? 2))
+              (should= false (perfect-square? 3))
+              (should= true (perfect-square? 4))
+              (should= true (perfect-square? 4))
+              )
+          (it "is fibonachi"
+              (should= true (is-fibonacci? 0))
+              (should= true (is-fibonacci? 1))
+              (should= true (is-fibonacci? 2))
+              (should= true (is-fibonacci? 3))
+              (should= false (is-fibonacci? 4))
+              (should= true (is-fibonacci? 5))
+              (should= false (is-fibonacci? 6))
+              (should= false (is-fibonacci? 7))
+              (should= true (is-fibonacci? 8))
+              (should= false (is-fibonacci? 9))
+              (should= true (is-fibonacci? 13))
+              )
+          (it "finds fibonachi"
+              (should= [] (find-fibonacci 0))
+              (should= [0] (find-fibonacci 1))
+              (should= [0 1] (find-fibonacci 2))
+              (should= [0 1 2] (find-fibonacci 3))
+              (should= [0 1 2 3] (find-fibonacci 4))
+              (should= [0 1 2 3] (find-fibonacci 5))
+              (should= [0 1 2 3 5] (find-fibonacci 6))
+              )
 
   (it "Solves #2"
-    (should= -1 (euler-2 -1)))
+    (should= 0 (euler-2 0))
+    (should= 0 (euler-2 1))
+    (should= 0 (euler-2 2))
+    (should= 2 (euler-2 3))
+    (should= 2 (euler-2 4))
+    (should= 2 (euler-2 5))
+    (should= 2 (euler-2 6))
+    (should= 2 (euler-2 7))
+    (should= 10 (euler-2 9))
+    (should= 4613732 (euler-2 4000000))
+      )
   )
 
-#_(run-specs)
+(run-specs)
--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,4 +1,16 @@
 (ns euler.level1.problem002)
 
+(defn perfect-square? [n]
+      (let [sqrt-n (Math/sqrt n)
+            int-sqrt-n (int sqrt-n)]
+           (== (* int-sqrt-n int-sqrt-n) n)))
+
+(defn is-fibonacci? [n]
+      (or (perfect-square? (+ (* 5 (* n n)) 4))
+          (perfect-square? (- (* 5(* n n)) 4))))
+
+(defn find-fibonacci [n]
+      (filter is-fibonacci? (range n)))
+
 (defn euler-2 [n]
-  )
+      (apply + (filter even? (find-fibonacci n))) )
